ccad5b4baca324a044e87a48afde8d2761fb16d9
Alexey Yerin <yerinalexey98fd@gmail.com>
2020-08-03 16:59:40 +0300

    Add Epic component

---
+++ src/Epic.tsx
@@ -0,0 +1,78 @@
+import React, { useState, ReactNode, useMemo } from "react";
+import { Epic, Tabbar } from "@vkontakte/vkui";
+import TabbarItem, {
+  TabbarItemProps,
+} from "@vkontakte/vkui/dist/components/TabbarItem/TabbarItem";
+
+interface EpicProps {
+  /**
+   * Default story (`View` or `Root`'s ID)
+   */
+  homeStory: string;
+
+  /**
+   * Bar defenition
+   */
+  tabbar: NavigatorTabbar;
+}
+
+interface NavigatorTabbarItem
+  extends Exclude<TabbarItemProps, "selected" | "onClick"> {
+  /**
+   * Icon to place alongside the text
+   */
+  icon: ReactNode;
+
+  /**
+   * Story this item belongs to
+   */
+  story: string;
+}
+type NavigatorTabbar = NavigatorTabbarItem[];
+
+type TabbarOnChange = (story: string) => void;
+
+/**
+ * Build an actual `Tabbar` from the defenition
+ */
+const buildTabbar = (
+  tabbar: NavigatorTabbar,
+  selectedStory: string,
+  onChange: TabbarOnChange
+) => {
+  return (
+    <Tabbar>
+      {tabbar.map(({ icon, story, ...rest }) => (
+        <TabbarItem
+          selected={selectedStory === story}
+          onClick={() => {
+            onChange(story);
+          }}
+          {...rest}
+        >
+          {icon}
+        </TabbarItem>
+      ))}
+    </Tabbar>
+  );
+};
+
+export const NavigatorEpic: React.FC<EpicProps> = ({
+  homeStory,
+  tabbar,
+  children,
+}) => {
+  const [story, setStory] = useState(homeStory);
+
+  // Build tabbar
+  const builtTabbar = useMemo(() => buildTabbar(tabbar, story, setStory), [
+    story,
+    setStory,
+  ]);
+
+  return (
+    <Epic activeStory={story} tabbar={builtTabbar}>
+      {children}
+    </Epic>
+  );
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // Components
 export { NavigatorRoot as Root } from "./Root";
 export { NavigatorView as View } from "./View";
+export { NavigatorEpic as Epic } from "./Epic";
 
 // Hook and HOC
 export * from "./api";
