c2d301fc80d33af7c5f0bf0b5f03142ea3c9cefb
Alexey Yerin <yerinalexey98fd@gmail.com>
2020-10-09 20:32:07 +0300

    fix: make project build

--- src/context.ts
+++ src/context.ts
@@ -3,6 +3,7 @@ import { createContext } from "react";
 
 export const EpicContext = createContext<EpicState>({
   changeStory: () => {},
+  setTabbarVisibility: () => {},
 });
 
 export const RootContext = createContext<RootState>({
